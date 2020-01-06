1bf5b9e9431c47a10a8e26bf946007e0547f09d3
Steven <jgu8@u.rochester.edu>
2020-01-05 22:15:00 -0500

    word count function

--- wordcount/wordcount/views.py
+++ wordcount/wordcount/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import operator
 
 def home (request):
     return render(request, 'home.html')
 
 def count(request):
-    return render(request, 'count.html')
+    ft = request.GET['fulltext']
+    wordList = ft.split()
+    dictionary = {}
+
+    for x in wordList:
+        if x in dictionary:
+            dictionary[x] +=1
+        else:
+            dictionary[x] = 1
+
+    sortedWords = sorted(dictionary.items(), key=operator.itemgetter(1), reverse = True);
+    return render(request, 'count.html', {'enteredText': ft, 'count':len(wordList), 'dic':sortedWords})
